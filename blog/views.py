8a442d510bb1df62bc6395dc71f3071f8691ab40
Ali <ali.jafari20@gmail.com>
2017-05-12 13:03:01 +0430

    login/signup view and Json response for remote app added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
 from .forms import *
+from django.http import JsonResponse 
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
 
 def post_list(request):
     posts = Post.objects.filter(post_status = "published")
@@ -14,7 +17,7 @@ def addGroup(request):
     form = form_Group()
     # if form.is_valid():
     return render(request , "blog/base.html" , {"form":form})
-
+    
 def post_detail(request,idd):
     post = Post.objects.filter(post_id = idd)
     comments = Comment.objects.filter(active = True , postID = post[0].post_id )
@@ -26,3 +29,32 @@ def post_detail(request,idd):
         postid = Post.objects.filter(post_id = idd)
         new_comment , created = Comment.objects.get_or_create(email=email , name = name , Text = text , postID = postid[0] )
     return render(request,'blog/post/detail.html',{'post':post[0],'comments':comments , 'form':form})
+
+@csrf_exempt
+def Narlogin(request):
+    UserName = request.POST['nuser']
+    PassWord = request.POST['npass']
+    user = members.objects.filter(userName= UserName ,password = PassWord)
+    if len(user)>0:
+        return JsonResponse({'Status':'OK','Token':user[0].Token, },encoder=JSONEncoder)
+    else:
+        return JsonResponse({'Status':'FAILED','ERROR':'0x0001' },encoder=JSONEncoder)
+
+@csrf_exempt
+def NarSignUp(request):
+    UserName = request.POST['nuser']
+    PassWord = request.POST['npass']
+    email    = request.POST['nemail']
+    dispusn  = request.POST['ndispn']
+    grp      = request.POST['ngroup']
+    ngroup = NarGroups.objects.filter(Name = grp)
+
+    user = members.objects.filter(userName= UserName)
+    if len(user)>0:
+        return JsonResponse({'Status':'FAILED','ERROR':'0x0002', },encoder=JSONEncoder)
+    else:
+        new_member , created = members.objects.get_or_create(email=email , userName = UserName , password = PassWord , DisplayUserName = dispusn , Group = ngroup[0] )
+        if created:
+            return JsonResponse({'Status':'FAILED','ERROR':'0x0001' },encoder=JSONEncoder)
+        else:
+            return JsonResponse({'Status':'FAILED','Message':'unexpected error accured!',}, encoder=JSONEncoder)
